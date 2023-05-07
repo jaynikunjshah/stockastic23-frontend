8b56eac5173f67de96deedd93bd3e701b5988801
Shantanu <shantanukhoslas@gmail.com>
2023-05-07 13:06:34 +0530

    finished with event timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import React from 'react';
+import { events } from "../constants";
+import "./styles/EventsTimeline.css"
+import {Tilt} from 'react-tilt';
+
+
+
+const EventCard = ({index, image, title, description, dates}) => {
+  return(
+    <div className="">
+      <div className="">
+        <Tilt
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 150
+          }}
+          className=" min-w-[400px] event-gradient p-5 rounded-2xl shadow-md sm:w-[260px] w-full h-full"
+          >
+
+
+          <div className="relative w-full rounded-xl">
+            <img
+              src={image}
+              className="w-full h-full object-cover rounded-full bg-black"
+              />
+
+
+          </div>
+
+          <div className="mt-5">
+            <h3 className="text-white font-extrabold text-[25px] ">{title}</h3>
+            <p className="mt-2 text-white font-bold text-[15px] text-secondary ">{description}</p>
+            <p className="mt-2 text-white font-bold text-[14px] text-secondary ">{dates}</p>
+          </div>
+        </Tilt>
+      </div>
+    </div>
+  )
+}
+
 
 function Timeline() {
   return (
-    <div>
-      
-    </div>
+    <div
+      id="Team"
+      className="my-[10%]"
+      >
+
+
+
+      <div
+
+        className={`mt-20 flex flex-wrap gap-10`}>
+
+          <div
+            className="my-20 flex align-center justify-center flex-wrap gap-20 mx-auto"
+            >
+            {events.map((event, index) => {
+             return (
+               <EventCard
+                 key={`events-${index}`}
+                 index={index}
+                 image={event.image}
+                 title={event.title}
+                 description={event.description}
+                 dates={event.dates}
+               />
+             );
+           })}
+          </div>
+
+
+        </div>
+
+
+</div>
+
   )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+export const events = [
+  {
+    image : "/sign-form_1.svg",
+    title : "Registration Day",
+    description: "Registration day for an event: the anticipation and buzz builds as participants check-in and prepare to engage." ,
+    dates: "From -  2/04/23  To - 3/04/23"
+
+  },
+  {
+    image : "/quiz_1.svg",
+    title : "Quiz 1",
+    description : "Quiz for an event: a fun and competitive way to test knowledge, engage participants, and award prizes.",
+    dates: "At - 4/04/23"
+
+  },
+  {
+    image : "/six_1.svg",
+    title : "Event Day",
+    description : "Event day: a culmination of planning, energy, and excitement as attendees come together to celebrate.",
+    dates: "At - 6/06/23" ,
+
+  },
+];
