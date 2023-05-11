2d39895167c1b9e708a1959f31f375c4d5e8021d
Shantanu <shantanukhoslas@gmail.com>
2023-05-11 23:21:49 +0530

    timeline fixed

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 import { events } from "../constants";
 import "./styles/EventsTimeline.css"
 
