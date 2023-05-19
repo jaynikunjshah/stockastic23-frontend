52dec3d5d74317bd42ab42cbe691ea6407b9619a
Kamil Dehliwala <114414598+kamil26300@users.noreply.github.com>
2023-05-20 00:19:32 +0530

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -22,7 +22,7 @@ function SignUp() {
       .required("Password confirm is a required field"),
     regNo: Yup.string()
       .required("Register Number is a required field")
-      .matches("^2[0-4]B[A-Z]{2}[0-4][0-9]{3}$", "Invalid Register Number"),
+      .matches("^2[0-4][A-Z]{3}[0-4][0-9]{3}$", "Invalid Register Number"),
   });
   return (
     <div className="items-center tracking-[1px] text-[#fff] flex justify-center bg-[#0F0F0F]">
@@ -61,93 +61,94 @@ function SignUp() {
           handleSubmit,
         }) => (
           <div className="login grid md:grid-cols-2 w-[100%] h-[100%]">
-            <div className="form w-100 text-center m-4">
-              <a href="#" className="flex">
-                <img className="w-12" src="logo.svg" alt="" />
-                <div className="my-auto text-[#5FBDC8]">Stockastics</div>
-              </a>
+						<div className="form w-100 text-center m-4">
+							<a href="#" className="flex">
+								<img className="w-12" src="logo.svg" alt="" />
+								<div className="my-auto text-[#5FBDC8]">Stockastics</div>
+							</a>
 
-              <form noValidate onSubmit={handleSubmit}>
-                <span className="block font-[500] text-2xl mt-9 mb-3">
-                  CREATE AN ACCOUNT
-                </span>
-                <span className="block text-[#858585]">Sign up now...</span>
-                <input
-                  type="text"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  placeholder="Enter VIT Email ID"
-                  className="form-control inp_text p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
-                  id="email"
-                />
-                <p className="error mb-[10px] text-left text-red-500 text-[10px]">
-                  {errors.email && touched.email && errors.email}
-                </p>
-                <input
-                  type="text"
-                  name="regNo"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.regNo}
-                  placeholder="Enter Registration Number"
-                  className="form-control inp_text p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
-                  id="regNo"
-                />
-                <p className="error mb-[10px] text-left text-red-500 text-[10px]">
-                  {errors.regNo && touched.regNo && errors.regNo}
-                </p>
-                <input
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                  placeholder="Enter Password"
-                  className="form-control inp_text p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
-                  id="password"
-                />
-                <p className="error mb-[10px] text-left text-red-500 text-[10px]">
-                  {errors.password &&
-                    touched.password &&
-                    errors.password}
-                </p>
-                <input
-                  type="password"
-                  name="passwordConfirm"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.passwordConfirm}
-                  placeholder="Confirm your password"
-                  className="form-control inp_text p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
-                  id="passwordConfirm"
-                />
-                <p className="error mb-[10px] text-left text-red-500 text-[10px]">
-                  {errors.passwordConfirm && touched.passwordConfirm && errors.passwordConfirm}
-                </p>
-                <button
-                  type="submit"
-                  className="bg-[#7353BA]  mx-[10%] w-[80%] px-4 py-3 mt-4 rounded-xl mb-6 hover:opacity-75"
-                >
-                  Create Account
-                </button>
-                <a href="#">
-                  <button
-                    type="button"
-                    className="bg-[#1E1B1E]  mx-[10%] w-[80%] px-4 py-3 rounded-xl mb-[30px] hover:ring hover:ring-violet-100"
-                  >
-                    Sign In
-                  </button>
-                </a>
-              </form>
-              <a href="mailto:DM@gmail.com" className="flex mt-[4.3%] bottom-4">
-                <img src="gmail-grey.svg" alt="gmail" />
-                <div className="ms-2 my-auto">DM@gmail.com</div>
-              </a>
-            </div>
-            <div className="bg-[#7353BA] hidden md:flex m-4 rounded-e-2xl"><img className="mx-auto" src="SignUpPic.svg" alt="" /></div>
-          </div>
+							<form noValidate onSubmit={handleSubmit}>
+								<span className="block font-[500] text-2xl mt-[60px] mb-3">
+									CREATE AN ACCOUNT
+								</span>
+								<span className="block text-[#858585]">Sign up now...</span>
+								<input
+									type="name"
+									name="name"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.name}
+									placeholder="Enter Name"
+									className="form-control inp_text p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
+									id="name"
+								/>
+								<p className="error mb-[10px] text-left text-red-500 text-[10px]  ms-[10%]">
+									{errors.name && touched.name && errors.name}
+								</p>
+								<input
+									type="email"
+									name="email"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.email}
+									placeholder="Enter VIT Email ID"
+									className="form-control inp_text p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
+									id="email"
+								/>
+								<p className="error mb-[10px] text-left text-red-500 text-[10px] ms-[10%]">
+									{errors.email && touched.email && errors.email}
+								</p>
+								<input
+									type="registration_number"
+									name="registration_number"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.registration_number}
+									placeholder="Enter Registration Number"
+									className="form-control inp_text p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
+									id="registration_number"
+								/>
+								<p className="error mb-[10px] text-left text-red-500 text-[10px]  ms-[10%]">
+									{errors.registration_number &&
+										touched.registration_number &&
+										errors.registration_number}
+								</p>
+								<input
+									type="password"
+									name="password"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.password}
+									placeholder="Enter password"
+									className="form-control p-[10px] text-[14px] rounded-xl my-[15px] bg-[#1E1B1E]  mx-[10%] w-[80%]"
+								/>
+								<p className="error mb-[40px] text-left text-red-500 text-[10px]  ms-[10%]">
+									{errors.password && touched.password && errors.password}
+								</p>
+								<button
+									type="submit"
+									className="bg-[#7353BA]  mx-[10%] w-[80%] px-4 py-3 mt-4 rounded-xl mb-6 hover:opacity-75"
+								>
+									Create Account
+								</button>
+								<a href="#">
+									<button
+										type="button"
+										className="bg-[#1E1B1E]  mx-[10%] w-[80%] px-4 py-3 rounded-xl mb-[30px] hover:ring hover:ring-violet-100"
+									>
+										Sign In
+									</button>
+								</a>
+							</form>
+							<a href="mailto:DM@gmail.com" className="flex mt-[4.3%] bottom-4">
+								<img src="gmail-grey.svg" alt="gmail" />
+								<div className="ms-2 my-auto">DM@gmail.com</div>
+							</a>
+						</div>
+						<div className="bg-[#7353BA] hidden md:flex m-4 rounded-e-2xl ">
+							<img className="mx-auto" src="SignUpPic.svg" alt="" />
+						</div>
+					</div>
         )}
       </Formik>
     </div>
