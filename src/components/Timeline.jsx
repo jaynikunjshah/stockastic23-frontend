ab4f81964a60fe48c7e1ac673cd672308c67eb98
Kamil Dehliwala <114414598+kamil26300@users.noreply.github.com>
2023-05-20 00:21:39 +0530

    Update Timeline.jsx

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,66 +1,59 @@
-import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import React from "react";
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 import { events } from "../constants";
-import "./styles/EventsTimeline.css"
-
-
+import "./styles/EventsTimeline.css";
 
 const EventCard = ({ event }) => (
-  <VerticalTimelineElement
-    contentStyle={{
-      background: '#1d1836',
-      color: '#fff'
-    }}
-    contentArrowStyle={{
-      borderRight: '7px solid #232631'
-    }}
-    date={event.date}
-    iconStyle={{
-      background: event.iconBg
-    }}
-    >
-    <div>
-      <h3 className="text-white text-[24px] font-bold">
-        {event.title}
-      </h3>
-    </div>
+	<VerticalTimelineElement
+		contentStyle={{
+			background: "#7353BA",
+			color: "#fff",
+		}}
+		contentArrowStyle={{
+			borderRight: "7px solid #232631",
+		}}
+		date={event.date}
+		iconStyle={{
+			background: event.iconBg,
+		}}
+	>
+		<div>
+			<h3 className="text-white text-[24px] font-bold">{event.title}</h3>
+		</div>
 
-    <ul className="mt-5 list-disc ml-5 space-y-2">
-      {event.points.map((point, index) => (
-        <li
-          key={`event-point-${index}`}
-          className="text-white-100 text-[14px] pl-1 tracking-wider"
-          >
-          {point}
-        </li>
-      ))}
-    </ul>
-
-  </VerticalTimelineElement>
+		<ul className="mt-5 list-disc ml-5 space-y-2">
+			{event.points.map((point, index) => (
+				<li
+					key={`event-point-${index}`}
+					className="text-white-100 text-[14px] pl-1 tracking-wider"
+				>
+// 					{point}
+				</li>
+			))}
+		</ul>
+	</VerticalTimelineElement>
 );
-
 
 function Timeline() {
-  return (
-    <div
-      id="Team"
-      className="my-[10%]"
-      >
-
-      <div className="mt-20 flex flex-col">
-        <VerticalTimeline layout="1-column">
-          {events.map((event, index) => (
-            <EventCard key={index} event={event} />
-          ))}
-        </VerticalTimeline>
-      </div>
-
-
-</div>
-
-);
+	return (
+		<div id="Team" className="my-[10%]">
+			<div className="text-white text-6xl text-center">
+				Timeline Of The Event{" "}
+			</div>
+			<div className="mt-20 flex flex-col">
+				<VerticalTimeline layout="1-column">
+					{events.map((event, index) => (
+						<EventCard key={index} event={event} />
+					))}
+				</VerticalTimeline>
+			</div>
+		</div>
+	);
 }
 
-export default Timeline
+export default Timeline;
