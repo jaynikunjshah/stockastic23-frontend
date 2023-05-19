1f8926c89bbe88a5f88753571da430195699a005
Kamil Dehliwala <114414598+kamil26300@users.noreply.github.com>
2023-05-20 00:07:10 +0530

    Update FAQs.jsx

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -3,57 +3,57 @@ import './FAQs.css'
 
 function FAQs() {
   return (
-    <div className="grid mx-auto grid-cols-7 w-2/3 pt-6 mb-[90px] place-items-center h-max">
+    <div className="grid mx-auto grid-cols-7 text-white w-2/3 pt-6 mb-[90px] place-items-center h-max">
       <div className="container mx-auto col-span-7 lg:col-span-3">
-        <h1 className="text-6xl font-medium text-center title-font text-[#003F63] mb-6">
+        <h1 className="text-6xl font-medium text-center title-font mb-6">
           FAQ's
         </h1>
-        <div className="w-full mx-auto my-2">
+        <div className="w-full mx-auto my-2 ">
           {/* Question 1 */}
           <details className="mt-1 w-full">
-            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#D9D9D9] p-4">
+            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#1E1B1E] p-4">
               1. Question 1
             </summary>
-            <div className="bg-white font-medium p-2 pb-0 tracking-[0.5px]">
+            <div className="bg-[#1E1B1E] font-medium p-2 pb-0 tracking-[0.5px]">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi possimus sequi reiciendis officiis, minima ducimus iste aliquam quo! Illum assumenda deserunt voluptatum porro autem dignissimos et esse numquam commodi maxime?
             </div>
           </details>
 
           {/* Question 2 */}
           <details className="mt-4 w-full">
-            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#D9D9D9] p-4">
+            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#1E1B1E] p-4">
               2. Question 2
             </summary>
-            <div className="bg-white font-medium p-2 tracking-[0.5px]">
+            <div className="bg-[#1E1B1E] font-medium p-2 tracking-[0.5px]">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi possimus sequi reiciendis officiis, minima ducimus iste aliquam quo! Illum assumenda deserunt voluptatum porro autem dignissimos et esse numquam commodi maxime?
             </div>
           </details>
 
           {/* Question 3 */}
           <details className="mt-4 w-full">
-            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#D9D9D9] p-4">
+            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#1E1B1E] p-4">
               3. Question 3
             </summary>
-            <div className="bg-white font-medium p-2 tracking-[0.5px]">
+            <div className="bg-[#1E1B1E] font-medium p-2 tracking-[0.5px]">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi possimus sequi reiciendis officiis, minima ducimus iste aliquam quo! Illum assumenda deserunt voluptatum porro autem dignissimos et esse numquam commodi maxime?
             </div>
           </details>
           {/* Question 4 */}
           <details className="mt-4 w-full">
-            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#D9D9D9] p-4">
+            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#1E1B1E] p-4">
               4. Question 4
             </summary>
-            <div className="bg-white font-medium p-2 tracking-[0.5px]">
+            <div className="bg-[#1E1B1E] font-medium p-2 tracking-[0.5px]">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi possimus sequi reiciendis officiis, minima ducimus iste aliquam quo! Illum assumenda deserunt voluptatum porro autem dignissimos et esse numquam commodi maxime?
             </div>
           </details>
 
           {/* Question 5 */}
           <details className="mt-4 w-full">
-            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#D9D9D9] p-4">
+            <summary className="questions rounded-[15px] font-semibold tracking-[0.7px] list-none bg-[#1E1B1E] p-4">
               5. Question 5
             </summary>
-            <div className="bg-white font-medium p-2 tracking-[0.5px]">
+            <div className="bg-[#1E1B1E] font-medium p-2 tracking-[0.5px]">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi possimus sequi reiciendis officiis, minima ducimus iste aliquam quo! Illum assumenda deserunt voluptatum porro autem dignissimos et esse numquam commodi maxime?
             </div>
           </details>
