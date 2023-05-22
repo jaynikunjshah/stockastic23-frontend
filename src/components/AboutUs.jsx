93f1cc7b54d30836793988bd6b3421bf71ef55f7
Kamil Dehliwala <kamil.dehliwala2021@vitstudent.ac.in>
2023-05-22 23:56:32 +0530

    a

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,7 @@ function AboutUs() {
 		<div id="About-Us" className="grid lg:grid-cols-3 w-4/5 mx-auto mt-[100px] mb-[200px] text-white rounded-xl bg-[#7353BA] p-9 tracking-[1px]">
 			<img className="lg:block hidden translate-x-[-7rem] mx-auto h-full w-full" src="phone.svg" alt="phone.svg" />
 			<div className="col-span-2 lg:translate-x-[-4rem]">
-        <div className="mb-6 text-3xl text-[#d6d1d1]">About <span className="text-4xl font-bold text-[#0f1020]">STOCKASTICS</span></div>
+        <div className="mb-6 text-3xl text-[#d6d1d1]">About <span className="text-4xl font-bold text-[#0f1020]">STOCKASTIC</span></div>
 				<div className="text-xl text-[#d6d1d1]">
 					Join us for an exciting two-day online event hosted by Dream Merchants
 					in collaboration with VIT Vellore, featuring a stock market simulator.
